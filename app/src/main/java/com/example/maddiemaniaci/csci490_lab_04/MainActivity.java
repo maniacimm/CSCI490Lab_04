24dc42006139e6884dc5d9731fa3ea72a555a5c7
Madison Maniaci <maniacimm@gmail.com>
2018-04-15 21:23:27 -0400

    url connection lab

--- app/src/main/java/com/example/maddiemaniaci/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/example/maddiemaniaci/csci490_lab_04/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.maddiemaniaci.csci490_lab_04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public String createURL(String string) throws IOException {
+        URL url = new URL(string);
+        url.openConnection();
+
+        InputStream in = url.openStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        String data = reader.readLine();
+        Log.i("Data", data);
+        return data;
     }
 }
