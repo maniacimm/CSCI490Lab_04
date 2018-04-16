dd3c08699b6310a9c49aefa7c00caf616c79b535
Madison Maniaci <maniacimm@gmail.com>
2018-04-15 21:01:06 -0400

    webview lab

--- app/src/main/java/com/example/maddiemaniaci/csci490_lab_04/MainActivity.java
+++ app/src/main/java/com/example/maddiemaniaci/csci490_lab_04/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.maddiemaniaci.csci490_lab_04;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+
+        webview.loadUrl("http://www.youtube.com");
     }
 }
